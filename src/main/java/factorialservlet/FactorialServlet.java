edb63559425e8de274f3aa9f574483dd13e0cdf3
shreegowri230-png <shreegowri230@gmail.com>
2026-05-05 18:02:56 +0530

    Lab 6a

---
+++ src/main/java/factorialservlet/FactorialServlet.java
@@ -0,0 +1,27 @@
+package factorialservlet;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/factorial")   // URL mapping
+public class FactorialServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        int num = Integer.parseInt(request.getParameter("num"));
+        long fact = 1;
+
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+
+        out.println("<html><body>");
+        out.println("<h2>Factorial of " + num + " is: " + fact + "</h2>");
+        out.println("</body></html>");
+    }
+}
